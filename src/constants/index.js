f0d139b17f134b1566f225f238e9687412799c74
Alex Farouz <alexfarouz@gmail.com>
2024-08-13 16:51:43 -0400

    Modify projects section

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,9 @@
 import project1 from "../assets/projects/project-1.png";
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.png";
-import project4 from "../assets/projects/project-4.jpg";
+import project2 from "../assets/projects/project-2.png";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.png";
 import project5 from "../assets/projects/project-5.jpg"
+import project6 from "../assets/projects/project-6.jpg"
 import kruskalExe from "../assets/projects/Kruskal's Simulator.exe";
 
 export const BIO_CONTENT = `Hi! My name is Alex and I am a third-year Computer Science student at George Mason University looking to break in to tech as a Software Engineer. 
@@ -67,10 +68,21 @@ export const EXPERIENCES = [
 
 export const PROJECTS = [
   {
-    title: "Pantry Tracker",
+    title: "AI Assistant",
     image: project1,
+    description: `Constructed a Next.js RAG AI chatbot to help users with general purpose questions, but more specifically help aspiring SWEs break into tech.
+      The frontend was built with React, Next.js and TailwindCSS, while the backend was built using Python and Langchain for the RAG. Leveraged JWT token generation
+      from the Clerk API with a Firebase integration to store data. Utilized Langchain and Pinecone's vector database to allow for OpenAI GPT-4o-mini to provide more accurate
+      answers based on the given context. `,
+    technologies: ["Langchain", "Pinecone", "Firebase", "OpenAI", "Next.js"],
+    websiteLink: "https://ai-customer-support-three-phi.vercel.app/"
+  },
+
+  {
+    title: "Pantry Tracker",
+    image: project2,
     description: `Built a Next.js pantry tracker app to track a user's pantry inventory. Features the react-camera module to 
-      take a picture of a pantry item which is sent to the OpenAI GPT 4o mini model to analyze the image and add it to the users inventory. Based on a users inventory, the
+      take a picture of a pantry item which is sent to the OpenAI GPT-4o-mini model to analyze the image and add it to the users inventory. Based on a users inventory, the
       same OpenAI model generates a recipe with detailed duration, ingredients and instructions. Frontend built with Next.js + React and backend built with Firebase,
       Javascript. Utilized RESTful API's from OpenAI, Clerk for user login and Unsplash for image fetching. Holds 100+ users. `,
     technologies: ["Next.js", "React", "Firebase", "Javascript", "OpenAI"],
@@ -79,7 +91,7 @@ export const PROJECTS = [
 
   {
     title: "Sorting Visualizer",
-    image: project2,
+    image: project3,
     description:
       `A fully functional website used to visualize sorting algorithms such as merge sort, quick sort, heap sort and bubble sort. Allows for 
       adjustable number of data and adjustable sorting speed. Holds 200+ users composed of fellow George Mason University students from Data Structures and Algorithms course
@@ -90,7 +102,7 @@ export const PROJECTS = [
   
   {
     title: "Kruskal's Algorithm Simulator",
-    image: project3,
+    image: project4,
     description:
       `A visual simulator for Kruskal's Minimum Spanning Tree Algorithm fully implemented in Java. Includes a Swing GUI with editable
       graph nodes and graph edges to customize simulation for added capability. Utilized several self-implemented data structures such as graph, 
@@ -100,7 +112,7 @@ export const PROJECTS = [
   },
   {
     title: "Minesweeper Game",
-    image: project4,
+    image: project5,
     description:
       "A comprehensive Minesweeper Game complete with varying sizes and difficulties. Fully implemented in Java with a GUI using Java AWT and JUnit testing. ",
     technologies: ["Java", "Java AWT", "JUnit"],
@@ -108,7 +120,7 @@ export const PROJECTS = [
   },
   {
     title: "Heart Disease Detector",
-    image: project5,
+    image: project6,
     description: `Created an interactive web-app to detect heart disease based on 13 different parameters such as age, gender, cholesterol levels and more.
       Implemented K Nearest Neighboors (KNN) machine learning algorithm by referencing 5000+ cases of heart disease by a csv file from kaggle.`,
     technologies: ["Python", "HTML", "CSS", "JavaScript", "Flask"],
